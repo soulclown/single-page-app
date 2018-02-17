a77b1590841bfc6cca41e53c9678cd8e3244edf6
Andrea De Luisi <andrea.deluisi@everskill.de>
2018-02-17 23:07:47 +0100

    rate-weight for beard style

--- utils/rate-algorithm.js
+++ utils/rate-algorithm.js
@@ -1,16 +1,28 @@
 import * as constants from './constants.js'
 
 export const rateCalculator = (props) => {
-    return bodyShapeRate(props.body)
+    return bodyShapeRate(props.body) * beardStyleRate(props.beard)
 }
 
 const bodyShapeRate = (bodyShape) => {
     switch(bodyShape) {
         case constants.bodyShapes.NORMAL:
-            return 0.6
-        case constants.bodyShapes.MUSCLE:
-            return 0.4
-        case constants.bodyShapes.CHUBBY:
             return 0.8
+        case constants.bodyShapes.MUSCLE:
+            return 0.6
+        case constants.bodyShapes.CHUBBY:
+            return 1
+    }
+}
+const beardStyleRate = (beard) => {
+    switch(beard) {
+        case constants.beardStyles.NONE:
+            return 0.8
+        case constants.beardStyles.SMALL:
+            return 1
+        case constants.beardStyles.MIDDLE:
+            return 0.5
+        case constants.beardStyles.LARGE:
+            return 0.7
     }
 }
