c9331bba22a30ddc5939982cf27dcf034eee3cda
Andrea De Luisi <andrea.deluisi@everskill.de>
2018-02-17 13:40:04 +0100

    package mng + babel

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,24 @@ module.exports = {
     output: {
         filename: './bundle.js'
     },
-    watch: true
-};
+    watch: true,
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    {loader: "style-loader"},
+                    {loader: "css-loader"}
+                ]
+            }, {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env']
+                    }
+                }
+            }]
+    }
+}
