6ab8e3ed5f720988d9979bd5bfa8bce94afae269
Andrea De Luisi <andrea.deluisi@everskill.de>
2018-02-17 23:57:14 +0100

    rate-booster simpathy (+20%) and macho-man (-20%)

--- utils/rate-algorithm.js
+++ utils/rate-algorithm.js
@@ -7,37 +7,39 @@ export const rateCalculator = ({age, height, body, beard, eyeColor, hairLength }
         + beardStyleRate(beard)
         + eyeColorsRate(eyeColor)
         + hairLengthRate(hairLength)) / 6
-    if (boosterPavarotti({body, beard, eyeColor, hairLength }))   rate*=1.2   // +20%
+    if (boosterPavarotti({body, beard, eyeColor, hairLength }))     rate*=1.1   // +10%
+    if (boosterSympathy({body, beard, hairLength }))                rate*=1.2   // +20%
+    if (boosterMachoMan({body, hairLength, eyeColor, height }))     rate*=0.8   // -20%
     return rate
 }
 
 const bodyShapeRate = (bodyShape) => {
     switch(bodyShape) {
-        case constants.bodyShapes.NORMAL:   return 0.8
+        case constants.bodyShapes.NORMAL:   return 0.7
         case constants.bodyShapes.MUSCLE:   return 0.4
-        case constants.bodyShapes.CHUBBY:   return 1
+        case constants.bodyShapes.CHUBBY:   return 0.8
     }
 }
 const beardStyleRate = (beard) => {
     switch(beard) {
-        case constants.beardStyles.NONE:    return 0.8
-        case constants.beardStyles.SMALL:   return 1
-        case constants.beardStyles.MIDDLE:  return 0.5
-        case constants.beardStyles.LARGE:   return 0.7
+        case constants.beardStyles.NONE:    return 0.7
+        case constants.beardStyles.SMALL:   return 0.8
+        case constants.beardStyles.MIDDLE:  return 0.4
+        case constants.beardStyles.LARGE:   return 0.6
     }
 }
 const eyeColorsRate = (color) => {
     switch (color) {
         case constants.eyeColors.GREY:      return 0.6
-        case constants.eyeColors.BROWN:     return 0.8
-        case constants.eyeColors.GREEN:     return 0.7
+        case constants.eyeColors.BROWN:     return 0.7
+        case constants.eyeColors.GREEN:     return 0.5
         case constants.eyeColors.BLUE:      return 1
     }
 }
 const hairLengthRate = (length) => {
     switch(length) {
         case constants.hairLengths.BALD:    return 0.8
-        case constants.hairLengths.SHORT:   return 1
+        case constants.hairLengths.SHORT:   return 0.8
         case constants.hairLengths.MIDDLE:  return 0.7
         case constants.hairLengths.LONG:    return 0.4
     }
@@ -57,5 +59,22 @@ const boosterPavarotti = ({body, beard, eyeColor, hairLength }) => {
         && beard==constants.beardStyles.LARGE
         && eyeColor==constants.eyeColors.BROWN
         && body == constants.bodyShapes.NORMAL
+    )
+}
+
+const boosterSympathy = ({body, beard, hairLength }) => {
+    // Long hair, long beard and sweet belly
+    return (hairLength==constants.hairLengths.LONG
+        && beard==constants.beardStyles.LARGE
+        && body == constants.bodyShapes.CHUBBY
+    )
+}
+
+const boosterMachoMan = ({body, hairLength, eyeColor, height }) => {
+    // Muscles, long hair, Higher than 180cm, Muscle and blue Eye
+    return (height>180
+        && hairLength==constants.hairLengths.LONG
+        && eyeColor==constants.eyeColors.BLUE
+        && body == constants.bodyShapes.MUSCLE
     )
 }
