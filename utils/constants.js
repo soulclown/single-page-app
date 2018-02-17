2c85b59c35a70609d14e7d417f8d40772da83395
Andrea De Luisi <andrea.deluisi@everskill.de>
2018-02-17 14:21:27 +0100

    declaration of constants

---
+++ utils/constants.js
@@ -0,0 +1,31 @@
+export const hairLengths = {
+    LONG :  "LONG",
+    MIDDLE: "MIDDLE",
+    SHORT : "SHORT",
+    BALD:   "BALD"
+}
+export const hairColors = {
+    BLOND:  "BLOND",
+    BROWN:  "BROWN",
+    BLACK:  "BLACK",
+    RED:    "RED",
+    GREY:   "GREY"
+}
+export const eyeColors = {
+    BLUE:   "BLUE",
+    GREEN:  "GREEN",
+    BROWN:  "BROWN",
+    GREY:   "GREY"
+}
+export const beardStyle = {
+    NONE:   "NONE",
+    SMALL:  "SMALL",
+    MIDDLE: "MIDDLE",
+    LARGE:  "LARGE"
+}
+
+export const bodyShape = {
+    MUSCLE: "MUSCLE",
+    NORMAL: "NORMAL",
+    CHUBBY: "CHUBBY"
+}
